f10182fc538f67a57ea62811435d79a381c0a3ec
越智貴史 <takafumi_ochi@waku-2.com>
2019-10-07 19:32:21 +0900

    login form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form>
+          <div>
+            <label>メールアドレス</label>
+            <input type="email" name="email"/>
+          </div>
+          <div>
+            <label>パスワード</label>
+            <input type="email" name="email"/>
+          </div>
+          <button type="submit" >ログイン</button>
+        </form>
+      </div>
     </div>
   );
 }
