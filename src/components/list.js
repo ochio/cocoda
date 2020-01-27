85a3639258177f4032dcb973335451c986b6e8cc
ochi <ocos54ce@gmail.com>
2020-01-28 00:14:01 +0900

    json読み込み

--- src/components/list.js
+++ src/components/list.js
@@ -1,33 +1,46 @@
 import React, { Component } from 'react';
 import '../scss/list.scss';
 
-let json = require('../json/items.json');
-console.log(json);
+// let json = require('../json/items.json');
+const json = [
+	{
+		name: "ドリンクチケット",
+		img: "src/img/mv_stb.png"
+	},
+	{
+		name: "フードスイーツ",
+		img: "src/img/mv_stb.png"
+	},
+	{
+		name: "コンビニで使える",
+		img: "src/img/mv_stb.png"
+	},
+	{
+		name: "webサービス",
+		img: "src/img/mv_stb.png"
+	}
+]
 
+class Data extends Component{
+	render(){
+		const json = this.props.json;
+		return this.props.data.map((json, index) => <li key={index} >{json.name}</li>)
+	}
+}
 
 class List extends Component {
 	constructor(){
 		super();
 		this.state = ({
-			data:[]
+			data:json
 		})
 	}
 
-	setData(){
-		this.setState({data: json})
-		console.log(this.data);
-	}
-
   render(){
-		const data = (state) => {
-		const category = state.data.map((category, index) => <li key={index} >{category}</li>)
-		return <ul><category /></ul>
-		}
-
     return (
 			<div>
 				<h2>{this.props.title}</h2>
-				<data />
+				<Data data = {json}/>
 			</div>
 		)
   }
