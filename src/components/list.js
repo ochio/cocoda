4e98bd1080a79d5a360f395467a7e540ea7aa7d0
ochi <ocos54ce@gmail.com>
2020-02-04 00:16:43 +0900

    list作成

--- src/components/list.js
+++ src/components/list.js
@@ -1,30 +1,42 @@
 import React, { Component } from 'react';
 import '../scss/list.scss';
 
-// let json = require('../json/items.json');
+// import category1 from '../img/category_drink.png';
+// import category2 from '../img/category_food.png';
+// import category3 from '../img/category_shop.png';
+// import category4 from '../img/category_webService.png';
+
 const json = [
 	{
 		name: "ドリンクチケット",
-		img: "src/img/mv_stb.png"
+		img: "src/img/category_drink.png",
+		className: "drink"
 	},
 	{
 		name: "フードスイーツ",
-		img: "src/img/mv_stb.png"
+		img: "src/img/category_food.png",
+		className: "food"
 	},
 	{
 		name: "コンビニで使える",
-		img: "src/img/mv_stb.png"
+		img: "src/img/category_shop.png",
+		className: "shop"
 	},
 	{
 		name: "webサービス",
-		img: "src/img/mv_stb.png"
+		img: "src/img/category_webService.png",
+		className: "webService"
 	}
 ]
 
 class Data extends Component{
 	render(){
-		const json = this.props.json;
-		return this.props.data.map((json, index) => <li key={index} >{json.name}</li>)
+		return this.props.data.map((json, index) => 
+		<li key={index} className={`list list_${json.className}`}>
+			<div className="list_cover">
+				<p>{json.name}</p>
+			</div>
+		</li>)
 	}
 }
 
@@ -39,8 +51,10 @@ class List extends Component {
   render(){
     return (
 			<div>
-				<h2>{this.props.title}</h2>
-				<Data data = {json}/>
+				<h2 className="category">{this.props.title}</h2>
+				<ul className="listWrap">
+					<Data data = {json}/>
+				</ul>
 			</div>
 		)
   }
