8a06f5e004271595c1d2c32e7ab0390e034f5fd9
ochi <ocos54ce@gmail.com>
2020-02-24 14:32:08 +0900

    カテゴリ選択で絞り込み

--- src/pages/Message.js
+++ src/pages/Message.js
@@ -1,43 +1,54 @@
 import React, { Component } from 'react';
 import Slider from 'react-slick';
 
-import '../scss/message.scss';
-
 import Title from '../components/title';
 import Menu from '../components/Menu';
 import Process from '../components/Process';
+
+import CardsJson from '../json/cards';
 
 import Cheers from '../img/cardType_cheers.png';
 import Birthday from '../img/cardType_birthday.png';
 import Birthday02 from '../img/cardType_birthday02.png';
 import Wedding from '../img/cardType_wedding.png';
 import Foryou from '../img/cardType_foryou.png';
-import Foryou02 from '../img/cardType_foryou02.png';
+import Foryou02 from '../img/cardType_foryou02.png'; 
 import Congratulate from '../img/cardType_congratulate.png';
 import Cheers02 from '../img/cardType_cheers02.png';
+
+const imgAry = [Cheers,Birthday,Birthday02,Wedding,Foryou,Foryou02,Congratulate,Cheers02];
+const imgObj = {};
+
+let cateAry = []; 
+for(let i = 0; i < CardsJson.length; i++){
+	cateAry.push(CardsJson[i].name);
+	imgObj[`${cateAry[i]}`] = imgAry[i]
+}
 
 class Message extends Component {
 	constructor(){
 		super()
 		this.state = {
-			counts:{}
+			counts:{},
+			allSlides:[...cateAry],
+			initSlides:[...cateAry],
 		}
 	}
 
 	componentDidMount(){
-		const ele = document.getElementsByClassName("message_imgWrap");
+		const ele = document.getElementsByClassName("js_sort-list");
 		const ary = Array.from(ele);
 		let data = [];
 		for(let i = 0; i < ary.length; i++){
-			data.push(ary[i].dataset.type)
+			data.push(ary[i].dataset.sortItem)
 		}
 		let counts = {};
 		for(let i = 0; i < data.length; i++){
-			let key = data[i];
+			let key = data[i].replace(/\d/g, '');
 			counts[key] = (counts[key])? counts[key] + 1 : 1;
 		}
 		this.setState({
-			counts:{...counts}
+			counts:{...counts},
 		})
 	}
 
@@ -49,9 +60,26 @@ class Message extends Component {
 		return sum
 	}
 
-	handleDisplay(){
-		
-	}
+	filterList = () => {
+		let select;
+		let input = document.getElementsByClassName('js_nav-input');
+		let target = this.state.allSlides;
+    for(let i = 0; i < input.length; i++){
+			if(input[i].checked === true){
+				select = input[i].dataset.sortNav;
+			}
+		}
+		const updateSlide = target.filter((slide) => {
+			if(select === 'all') {
+				return	slide
+			}else{
+				console.log(slide.replace(/\d/g, '').toLowerCase())
+				console.log("select",select)
+				return slide.replace(/\d/g, '').toLowerCase() === select
+			}
+		})
+		this.setState({initSlides:updateSlide})
+  }
 
   render(){
 		const categorySetting = {
@@ -59,9 +87,9 @@ class Message extends Component {
 			infinite: false,
 			speed: 500,
 			slidesToShow: 3.9,
-			slidesToScroll: 1,
+			slidesToScroll: 1.5,
 		}
-
+		
 		const setting = {
 			dots: false,
 			infinite: false,
@@ -69,11 +97,11 @@ class Message extends Component {
 			slidesToShow: 3.2,
 			slidesToScroll: 1,
 			rows: 2,
+			className:"test"
 		}
-
+			
     return(
 			<div className="messageWrap">
-			{/* {console.log(this.state.birthday)} */}
 				<Title  name="ギフトカードを作成" />
 				<Process />
 				<section className="messageInner">
@@ -85,86 +113,56 @@ class Message extends Component {
 					</div>
 					<div className="cards">
 						<p className="heading">カードを選ぶ</p>
+						<form>
 						<section className="cards_lists">
-							
 							<Slider {...categorySetting}>
 								<label className="cards_select">
-									<input type="radio" name="category" className="cards_radio"/>
+									<input type="radio" name="category" className="cards_radio js_nav-input" data-sort-nav="all" onClick={this.filterList}/>
 									<div className="cards_list">全て{this.sum({...this.state.counts})}</div>
 								</label>
 								<label className="cards_select">
-									<input type="radio" name="category" className="cards_radio"/>
-									<div className="cards_list">誕生日{this.state.counts.birthday + this.state.counts.cheers}</div>
+									<input type="radio" name="category" className="cards_radio js_nav-input" data-sort-nav="birthday" onClick={this.filterList}/>
+									<div className="cards_list">誕生日{this.state.counts.Birthday}</div>
 								</label>
 								<label className="cards_select">
-									<input type="radio" name="category" className="cards_radio"/>
-									<div className="cards_list">結婚祝い{this.state.counts.wedding}</div>
+									<input type="radio" name="category" className="cards_radio js_nav-input" data-sort-nav="wedding" onClick={this.filterList}/>
+									<div className="cards_list">結婚祝い{this.state.counts.Wedding}</div>
 								</label>
 								<label className="cards_select">
-									<input type="radio" name="category" className="cards_radio"/>
-									<div className="cards_list">お見舞い{this.state.counts.congratulate}</div>
+									<input type="radio" name="category" className="cards_radio js_nav-input" data-sort-nav="congratulate" onClick={this.filterList}/>
+									<div className="cards_list">お見舞い{this.state.counts.Congratulate}</div>
 								</label>
 								<label className="cards_select">
-									<input type="radio" name="category" className="cards_radio"/>
-									<div className="cards_list">その他{this.state.counts.foryou}</div>
+									<input type="radio" name="category" className="cards_radio js_nav-input" data-sort-nav="foryou" onClick={this.filterList}/>
+									<div className="cards_list">その他{this.state.counts.Foryou}</div>
 								</label>
 							</Slider>
 						</section>
-						<form>
 							<Slider {...setting}>
-								<label className="message_imgWrap" data-type="cheers">
-									<input type="radio" name="type" className="message_radio"/>
-									<div className="message_innerImg">
-										<img className="message_img" src={Cheers} alt="Cheers"/>
-									</div>
-								</label>
-								<label className="message_imgWrap" data-type="birthday">
-									<input type="radio" name="type" className="message_radio"/>
-									<div className="message_innerImg">
-										<img className="message_img" src={Birthday} alt="Birthday"/>
-									</div>
-								</label>
-								<label className="message_imgWrap" data-type="birthday">
-									<input type="radio" name="type" className="message_radio"/>
-									<div className="message_innerImg">
-										<img className="message_img" src={Birthday02} alt="Birthday02"/>
-									</div>
-								</label>
-								<label className="message_imgWrap" data-type="wedding">
-									<input type="radio" name="type" className="message_radio"/>
-									<div className="message_innerImg">
-										<img className="message_img" src={Wedding} alt="Wedding"/>
-									</div>
-								</label>
-								<label className="message_imgWrap" data-type="foryou">
-									<input type="radio" name="type" className="message_radio"/>
-									<div className="message_innerImg">
-									<img className="message_img" src={Foryou} alt="Foryou"/>
-									</div>
-								</label>
-								<label className="message_imgWrap" data-type="foryou">
-									<input type="radio" name="type" className="message_radio"/>
-									<div className="message_innerImg">
-									<img className="message_img" src={Foryou02} alt="Foryou02"/>
-									</div>
-								</label>
-								<label className="message_imgWrap" data-type="congratulate">
-									<input type="radio" name="type" className="message_radio"/>
-									<div className="message_innerImg">
-									<img className="message_img" src={Congratulate} alt="Congratulate"/>
-									</div>
-								</label>
-								<label className="message_imgWrap" data-type="cheers">
-									<input type="radio" name="type" className="message_radio"/>
-									<div className="message_innerImg">
-									<img className="message_img" src={Cheers02} alt="Cheers02"/>
-									</div>
-								</label>
+								{this.state.initSlides.map((slide,index) => {
+									return (
+										<label className="message_imgWrap js_sort-list" data-sort-item={slide} key={index}>
+											<input type="radio" name="type" className="message_radio"/>
+											<div className="message_innerImg">
+												<img className="message_img" src={imgObj[slide]} alt={slide}/>
+											</div>
+										</label>
+									)
+								})
+								}
 							</Slider>
+							<label>
+								メッセージを入力
+								<input type="textarea" name="message" maxLength="400"　placeholder="全角400字以内(改行可)"/>
+							</label>
+							<label>
+								署名を入力
+								<input type="text" name="sign" maxLength="20" placeholder="全角20字以内"/>
+							</label>
 						</form>
 					</div>
-					<Menu />
 				</section>
+					<Menu />
 			</div>
 
     )
